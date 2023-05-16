846bc46123d55e5de738e14f13dfc1e2c4a348df
Hanna <hannadurham3@gmail.com>
2023-05-17 00:35:27 +0100

    homeworkSubmit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,75 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
+
+//============================================================
+
+
 function App() {
+
+
+const [tasks, setTasks] = useState([
+  {id:1, name: "Water plants", priority : true},
+  {id:2, name: "Go shopping", priority : false},
+  {id:3, name: "Make dinner", priority : true}
+])
+
+const [newTask, setNewTask] = useState("")
+
+//============================================================
+
+const listOfTasks = tasks.map((task) => {
+  return(
+    <li className={task.priority? "high" : "low"} key = 
+    {task.id}>{task.name}
+    {/* <button className={task.priority? "high" : "low"}></button> */}
+  <button onClick={() => deleteTask(task.id)}> Task Done</button>
+    </li>
+  )
+})
+
+const deleteTask = (parameterThatTakesInAnId) => {
+  console.log("Delete button clicked", parameterThatTakesInAnId)
+    
+const newTasks = tasks.filter((task) => task.id !== parameterThatTakesInAnId)
+setTasks(newTasks)
+}
+
+
+const handleTaskInput = (event) => {
+  setNewTask(event.target.value)
+}
+
+const saveNewTask = (event) => {
+  event.preventDefault()
+
+const newTaskObj = {id: Date.now(), name : newTask}
+const nextTasks = [... tasks, newTaskObj]
+setTasks(nextTasks)
+setNewTask("")
+
+}
+
+
+
+//============================================================
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <h1>Tasks Tae Be Done</h1>
+
+    <hr></hr>
+    <ul></ul>
+    {listOfTasks}
+
+
+  <form onSubmit={saveNewTask}>
+    <label htmlFor='new-task'>Add a new Task</label>
+    <input id='new-task' type='text' value={newTask} onChange={handleTaskInput}/>
+    <input type='submit' value="Save New Task"/>
+  </form>
+
+  </div>
   );
 }
 
